359b7e9652e72433aca76e9eb3e7fc16f9845e1a
Nikhil Sai Allam <nikhil.logicwind@gmail.com>
2022-01-28 19:08:33 +0530

    :artist: added routes

--- src/project/Home.jsx
+++ src/project/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import MainPage from './components/MainPage'
 import CustomizedTheme from './components/CustomizedTheme'
 
 export default function Home() {
   return (
     <Router>
-      <Switch>
+      <Routes>
         <CustomizedTheme>
           <Route exact path="/" component={MainPage}/>
         </CustomizedTheme>
-      </Switch>
+      </Routes>
     </Router>
   )
 }
